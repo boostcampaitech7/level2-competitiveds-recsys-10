5d6ec7926b05ec4244209f5f0a66443bbe0b2fc8
Jun <wns9096@naver.com>
2024-10-24 10:42:59 +0900

    feat: add cluster code

--- rentpredictor/preprocessor.py
+++ rentpredictor/preprocessor.py
@@ -1,6 +1,12 @@
 import numpy as np
 import pandas as pd
+import time
+import matplotlib.pyplot as plt
 from scipy.spatial import KDTree
+from sklearn.cluster import KMeans
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import silhouette_score, davies_bouldin_score
+
 
 class Preprocessor:
     """데이터 전처리를 수행하는 클래스입니다.
@@ -215,6 +221,152 @@ class Preprocessor:
             제거핧 열의 이름입니다.
         """
         self.all_df = self.all_df.drop(columns=column_names)
+
+    def find_optimal_clusters(self, features: np.ndarray, max_k: int, min_k: int, sample_size: float = 0.1, random_state=42) -> int:
+        """
+        엘보우 방법과 실루엣 스코어, davies_bouldin_score를 사용하여 최적의 클러스터 개수를 찾습니다.
+
+        Parameters
+        ----------
+        features : np.ndarray
+            클러스터링에 사용할 특징 데이터입니다..
+        max_k : int, optional
+            클러스터의 최대 개수입니다.
+        min_k : int, optional
+            클러스터의 최소 개수입니다.
+        sample_size : float, optional
+            샘플링 비율입니다. 기본값은 10%입니다.
+
+        Returns
+        -------
+        int
+            최적의 클러스터 개수입니다.
+        """
+        np.random.seed(random_state)
+        sample_indices = np.random.choice(features.shape[0], int(features.shape[0] * sample_size), replace=False)
+        sampled_features = features[sample_indices]
+
+        inertia = []
+        silhouette_scores = []
+        davies_bouldin_scores = []
+
+        for k in range(min_k, max_k + 1):
+            print(f"Clustering with k = {k} started at {time.strftime('%Y-%m-%d %H:%M:%S')}")
+            start_time = time.time()
+
+            kmeans = KMeans(n_clusters=k, random_state=42)
+            kmeans.fit(sampled_features)
+            inertia.append(kmeans.inertia_)
+
+            silhouette_score_value = silhouette_score(sampled_features, kmeans.labels_)
+            silhouette_scores.append(silhouette_score_value)
+
+            davies_bouldin_score_value = davies_bouldin_score(sampled_features, kmeans.labels_)
+            davies_bouldin_scores.append(davies_bouldin_score_value)
+
+            end_time = time.time()
+            print(f"Clustering with k = {k} finished at {time.strftime('%Y-%m-%d %H:%M:%S')} (Duration: {end_time - start_time:.2f} seconds)")
+
+        plt.figure(figsize=(15, 5))
+
+        plt.subplot(1, 3, 1)
+        plt.plot(range(min_k, max_k + 1), inertia, 'bx-')
+        plt.xlabel('Number of clusters')
+        plt.ylabel('Inertia')
+        plt.title('Elbow Method for Optimal k')
+
+        plt.subplot(1, 3, 2)
+        plt.plot(range(min_k, max_k + 1), silhouette_scores, 'bx-')
+        plt.xlabel('Number of clusters')
+        plt.ylabel('Silhouette Score')
+        plt.title('Silhouette Score for Optimal k')
+
+        plt.subplot(1, 3, 3)
+        plt.plot(range(min_k, max_k + 1), davies_bouldin_scores, 'bx-')
+        plt.xlabel('Number of clusters')
+        plt.ylabel('Davies-Bouldin Index')
+        plt.title('Davies-Bouldin Index for Optimal k')
+
+        plt.tight_layout()
+        plt.show()
+
+
+        optimal_k = range(min_k, max_k + 1)[silhouette_scores.index(max(silhouette_scores))]
+        print(f"Optimal k found: {optimal_k}")
+        return optimal_k
+
+    def add_location_based_cluster(self, train_df, test_df, max_k: int = 500, min_k: int = 450, sample_size: float = 0.1, random_state=42) -> int:
+        """
+        훈련 데이터의 위치(위도와 경도)를 기반으로 최적의 클러스터 ID를 찾고,
+        해당 결과를 바탕으로 테스트 데이터에 클러스터를 할당합니다.
+
+        Parameters
+        ----------
+        train_df : pd.DataFrame
+            훈련 데이터.
+        test_df : pd.DataFrame
+            테스트 데이터.
+        max_k : int, optional
+            클러스터의 최대 개수입니다.
+        min_k : int, optional
+            클러스터의 최소 개수입니다.
+        sample_size : float, optional
+            샘플링 비율입니다. 기본값은 10%입니다.
+        random_state : int, optional
+            랜덤 시드입니다. 기본값은 42입니다.
+
+        Returns
+        -------
+        int
+            최적의 클러스터 개수입니다.
+        """
+        np.random.seed(random_state)
+        train_location_features = train_df[['location_id']]
+
+        scaler = StandardScaler()
+        scaled_train_location_features = scaler.fit_transform(train_location_features)
+
+        optimal_k = self.find_optimal_clusters(scaled_train_location_features, max_k,   min_k, sample_size)
+
+        kmeans = KMeans(n_clusters=optimal_k, random_state=random_state)
+        kmeans.fit(scaled_train_location_features)
+
+        train_df['location_cluster'] = pd.Categorical(kmeans.labels_)
+        test_location_features = test_df[['location_id']]
+        scaled_test_location_features = scaler.transform(test_location_features)
+        test_df['location_cluster'] = pd.Categorical(kmeans.predict (scaled_test_location_features))
+
+        self.train_df = train_df
+        self.test_df = test_df
+
+        return optimal_k
+    
+    def add_min_distance_to_transfer_station(self) -> None:
+        """
+        각 아파트에서 가장 가까운 환승역까지의 거리를 추가합니다.
+        환승역은 위도와 경도가 동일한 역들로 정의됩니다.
+        """
+        transfer_station_groups = self.subway_df.groupby(['latitude', 'longitude']).size().reset_index(name='transfer_lines_count')
+        transfer_df = transfer_station_groups[transfer_station_groups['transfer_lines_count'] >= 2]
+        transfer_tree = KDTree(transfer_df[['latitude', 'longitude']])
+        id_to_distance = {id: transfer_tree.query([loc])[0][0] for loc, id in self.loc_to_id.items()}
+        self.all_df['min_distance_to_transfer_station'] = self.all_df['location_id'].map(id_to_distance)
+
+    def add_transfer_stations_within_radius(self, radius: float) -> None:
+        """
+        반경 내에 있는 환승역의 수를 추가합니다.
+        환승역은 위도와 경도가 동일한 역들로 정의됩니다.
+
+        Parameters
+        ----------
+        radius : float
+            반경 내 환승역을 계산할 반경 (km 단위).
+        """
+        transfer_station_groups = self.subway_df.groupby(['latitude', 'longitude']).size().reset_index(name='transfer_lines_count')
+        transfer_df = transfer_station_groups[transfer_station_groups['transfer_lines_count'] >= 2]
+        transfer_tree = KDTree(transfer_df[['latitude', 'longitude']])
+        id_to_transfer_count = {id: len(transfer_tree.query_ball_point(loc, r=radius)) for loc, id in self.loc_to_id.items()}
+        self.all_df[f'num_transfer_stations_within_{radius}'] = self.all_df['location_id'].map(id_to_transfer_count)
 
     def get_train_df(self) -> pd.DataFrame:
         """전처리된 학습 데이터 프레임을 반환합니다.
